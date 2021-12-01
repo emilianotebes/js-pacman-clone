27676e8196366e9ba8c6ec7ad3cabae514f87479
Emiliano Tebes <emiliano.tebes@despegar.com>
2021-12-01 16:43:38 -0300

    Initial

---
+++ src/app.js
@@ -0,0 +1,15 @@
+class App extends React.Component {
+    render() {
+        return (
+            <div className="shopping-list">
+                <h1>PAC MAN</h1>
+                <h1>Shopping List for {this.props.name}</h1>
+                <ul>
+                    <li>Instagram</li>
+                    <li>WhatsApp</li>
+                    <li>Oculus</li>
+                </ul>
+            </div>
+        );
+    }
+}
